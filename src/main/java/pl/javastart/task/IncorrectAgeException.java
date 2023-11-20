904befb138f72be43ff9c67dc0848f2863d7d25f
afir123 <kiri15@o2.pl>
2023-11-20 15:55:45 +0100

    zadanie v3

--- src/main/java/pl/javastart/task/IncorrectAgeException.java
+++ src/main/java/pl/javastart/task/IncorrectAgeException.java
@@ -3,8 +3,9 @@ package pl.javastart.task;
 public class IncorrectAgeException {
 
     static void checkAge(int age) {
-        if (age < 1)
+        if (age < 1) {
             throw new IllegalArgumentException("Wiek nie jest prawidłowy " + age);
+        }
     }
 }
 
--- src/main/java/pl/javastart/task/NameUndefinedException.java
+++ src/main/java/pl/javastart/task/NameUndefinedException.java
@@ -5,16 +5,18 @@ public class NameUndefinedException extends RuntimeException {
     private static final int MIN_LASTNAME_LENGTH = 2;
 
     static void checkFirstName(String firstName) {
-        if (firstName == null)
+        if (firstName == null) {
             throw new IllegalArgumentException("Imię nie jest prawidłowe " + firstName);
-        else if (firstName.length() < MIN_FIRSTNAME_LENGTH)
+        } else if (firstName.length() < MIN_FIRSTNAME_LENGTH) {
             throw new IllegalArgumentException("Imię powinno być dłuższe niż " + MIN_FIRSTNAME_LENGTH + " znaki");
+        }
     }
 
     static void checkLastName(String lastName) {
-        if (lastName == null)
+        if (lastName == null) {
             throw new IllegalArgumentException("Nazwisko nie jest prawidłowe " + lastName);
-        else if (lastName.length() < MIN_LASTNAME_LENGTH)
+        } else if (lastName.length() < MIN_LASTNAME_LENGTH) {
             throw new IllegalArgumentException("Nazwisko powinno być dłuższe niż " + MIN_LASTNAME_LENGTH + " znaki");
+        }
     }
 }
